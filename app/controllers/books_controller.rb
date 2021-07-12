9ffb48792d946c2332e2e1fc90668e19223e03f7
shoponcom001 <shopon.com001@gmail.com>
2021-07-12 12:27:26 +0000

    Update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,13 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
+    if @book.save 
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
 
   def index
@@ -20,7 +25,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.new
     @book1 = Book.find(params[:id])
-    @user = User.find(params[:id])
+    @user = @book1.user
   end
 
   def edit
@@ -28,9 +33,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render :edit
+    end
   end
 
   def destroy
